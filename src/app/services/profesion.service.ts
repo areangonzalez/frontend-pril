9dc9fe05789cecda24c6da0cbb1fd41520499af9
areangonzalez <areangonzalez@gmail.com>
2018-11-02 20:20:06 -0300

    TG-324 se configura los servicios de destinatario

--- src/app/services/profesion.service.ts
+++ src/app/services/profesion.service.ts
@@ -1,16 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from "../../environments/environment";
-import { httpOptions } from "../services/http-options.service";
+import { ApiService } from "../services/api.service";
 
 @Injectable()
 export class ProfesionService {
-    private url: string = environment.baseUrl;
 
-    constructor(private _http: HttpClient) { }
+    constructor(private _http: ApiService) { }
 
     listarProfesiones() {
-       return this._http.get(this.url + '/profesions');
+       return this._http.get('/profesions');
     }
 
 
