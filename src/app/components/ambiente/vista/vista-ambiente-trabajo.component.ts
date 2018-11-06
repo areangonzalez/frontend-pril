766c56097013c0c0f0f7286b114b2d9e666cf0d7
areangonzalez <areangonzalez@gmail.com>
2018-11-06 11:34:58 -0300

    TG-326 se aplica funcionalidad en los botones en la vista de un ambiente de trabajo

--- src/app/components/ambiente/vista/vista-ambiente-trabajo.component.ts
+++ src/app/components/ambiente/vista/vista-ambiente-trabajo.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
-import { Router, NavigationEnd, ActivatedRouteSnapshot } from '@angular/router';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { BreadcrumbsService } from "../../breadcrumbs/breadcrumbs.service";
+// services
+import { AmbienteTrabajoService } from "../../../services/ambiente-trabajo.service";
+import { OfertaService } from "../../../services/oferta.service";
+import { MensajesService } from "../../../services/mensajes.service";
 
 @Component({
     selector: 'ambiente-trabajo-vista',
@@ -8,30 +12,61 @@ import { BreadcrumbsService } from "../../breadcrumbs/breadcrumbs.service";
     styleUrls: ['./vista-ambiente-trabajo.css']
 })
 export class VistaAmbienteTrabajoComponent {
-    public ofertas = [
-        { fecha_inicial: "12/07/2018", puesto: "Chofer", dias_horarios: "Lunes a viernes 06hs a 08hs", estado: "Vacante", id:1 },
-        { fecha_inicial: "15/08/2018", puesto: "Limpieza", dias_horarios: "Lunes a viernes 21hs a 23hs", estado: "Ocupado", id: 2 },
-        { fecha_inicial: "07/09/2018", puesto: "Cajero", dias_horarios: "Lunes a viernes 08hs a 12hs", estado: "Finalizado", id: 3 }
-    ];
+    public listaOfertas:any;
+    private id:any;
+    public idAmbiente:number;
+    public ambiente:object;
 
     constructor(
         private _router: Router,
-        private _breadcrumbsService: BreadcrumbsService
+        private _route: ActivatedRoute,
+        private _breadcrumbsService: BreadcrumbsService,
+        private _ambienteTrabajoService: AmbienteTrabajoService,
+        private _ofertaService: OfertaService,
+        private _mensajeService: MensajesService
     ) { }
 
     ngOnInit() {
         // breadcrumbs Dinamico
         this._breadcrumbsService.store([{ label: 'Inicio', url: 'inicio', params: [] }, { label: 'Ambiente de trabajo', url: 'ambiente', params: [] }, { label: 'Ver', url: 'ambiente/vista', params: [] }]);
+        this.id = this._route.snapshot.paramMap.get('id');
+        if (this.id != undefined) {
+            this.idAmbiente = this.id;
+            this.buscarOfertas(this.id);
+            this.ambientePorId(this.id);
+        } else {
+            this._router.navigate(['ambiente']);
+        }
     }
 
     volver() {
         this._router.navigate(['ambiente']);
     }
 
-    agregarOferta() {
-        this._router.navigate(['ambiente', 'oferta', 'agregar']);
+    editarAmbiente(id) {
+        this._router.navigate(['ambiente', 'editar', id]);
     }
 
-    
+    agregarOferta(id) {
+        this._router.navigate(['ambiente', id, 'ofertas']);
+    }
+
+    private buscarOfertas(id){
+        this._ofertaService.listarOfertas(id).subscribe(
+            datos => {
+                this.listaOfertas = datos;
+            }, error => {
+                this._mensajeService.cancelado(error, [{name:''}]);
+            });
+    }
+
+    private ambientePorId(id) {
+        this._ambienteTrabajoService.ambientePorId(id).subscribe(
+            datos => {
+                this.ambiente = datos;
+            }, error => {
+                this._mensajeService.cancelado(error, [{ name: '' }]);                
+            });
+    }
 
 }
