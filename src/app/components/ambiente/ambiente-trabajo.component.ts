d391d8bd2ba00cf2fdbde0ecf08276f2bfd97f0b
areangonzalez <areangonzalez@gmail.com>
2018-12-21 13:57:30 -0300

    TG-456 se actualizan los datos del listado de ambiente de trabajo

--- src/app/components/ambiente/ambiente-trabajo.component.ts
+++ src/app/components/ambiente/ambiente-trabajo.component.ts
@@ -37,10 +37,54 @@ export class AmbienteTrabajoComponent implements OnInit {
     }
 
     private listaAmbientes() {
-        this._ambienteTrabajoService.listarAmbienteTrabajo().subscribe(
+        this._ambienteTrabajoService.listarAmbienteTrabajo()
+        .map(listaAmbientes => {
+          let listarDatos: any[] = [];
+          this.totalFiltrado = listaAmbientes['total_filtrado'];
+          // armo el listado de ambientes de trabajo
+          listaAmbientes['coleccion'].forEach(el => {
+            let direccion:string = '';
+            let telefonos:string = '';
+            // armo la direccion de ambiente de trabajo
+            direccion = el.lugar.localidad + ' - ' + el.lugar.calle + ' ' + el.lugar.altura;
+            direccion += (el.lugar.barrio != '') ? ' - Barrio: ' + el.lugar.barrio:'';
+            direccion += (el.lugar.escalera != '') ? ' - Esc/Mod: ' + el.lugar.escalera:'';
+            direccion += (el.lugar.piso != '') ? ' - Piso: ' + el.lugar.piso:'';
+            direccion += (el.lugar.depto != '') ? ' - Dpto: ' + el.lugar.depto:'';
+            // armo un listado de telefonos de ambiente de trabajo
+            telefonos = (el.telefono1 != '') ? el.telefono1 : '';
+            if (el.telefono2 != ''){
+              telefonos += (telefonos != '') ? ' / ' + el.telefono2 : el.telefono2;
+            }
+            if (el.telefono3 != ''){
+              telefonos += (telefonos != '') ? ' / ' + el.telefono3 : el.telefono3;
+            }
+            // armo el objeto del listado
+            listarDatos.push({
+              // datos del representante
+              representante: el.persona.apellido + ', ' + el.persona.nombre,
+              rep_telefono: el.persona.telefono,
+              rep_celular: el.persona.celular,
+              rep_email: el.persona.email,
+              //datos de ambiente
+              nombre: el.nombre,
+              fax: el.fax,
+              email: el.email,
+              cuit: el.cuit,
+              tipo_ambiente_trabajo: el.tipo_ambiente_trabajo,
+              estado: el.estado,
+              id: el.id,
+              direccion: direccion,
+              telefonos: telefonos
+            });
+
+          });
+          return listarDatos;
+        })
+        .subscribe(
             datos => {
-                this.ambientes = datos['coleccion'];
-                this.totalFiltrado = datos['total_filtrado'];
+                this.ambientes = datos;
+                //this.totalFiltrado = datos['total_filtrado'];
             }, error => {
                 this._mensajeService.cancelado(error, [{ name: '' }]);
             }
