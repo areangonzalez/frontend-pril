5f9807ff9c9c5d8de1bcd592d391b5273fbdf997
cep11p <cep11p@gmail.com>
2019-10-21 19:09:57 -0300

    se borra funcion e importaciones innecesarias

--- src/app/shared/formulario/ambiente-trabajo/datos-ambiente-trabajo-form.component.ts
+++ src/app/shared/formulario/ambiente-trabajo/datos-ambiente-trabajo-form.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup } from "@angular/forms";
 import { UtilService } from "../../../core/utils";
-// services
-import { TipoAmbienteTrabajoService } from "../../../core/services/tipo-ambiente-trabajo.service";
-import { MensajesService } from "../../../core/services/mensajes.service";
 
 @Component({
     selector: 'datos-ambiente-trabajo-form',
@@ -21,12 +18,9 @@ export class DatosAmbienteTrabajoFormComponent implements OnInit {
 
     constructor(
         private _utilService: UtilService,
-        private _mensajeService: MensajesService,
-        private _tipoAmbienteTrabajoService: TipoAmbienteTrabajoService
     ) { }
 
     ngOnInit() {
-        // this.listarTipoAmbienteTrabajo();
     }
 
     get ambiente(){ return this.datosAmbienteTrabajo.controls; }
@@ -49,14 +43,5 @@ export class DatosAmbienteTrabajoFormComponent implements OnInit {
         if (!this._utilService.validarNumero(event)) {
             this.datosAmbienteTrabajo.controls.cuit.setValue(nro_docuemnto.substring(0, nro_docuemnto.length - 1));
         }
-    }
-
-    private listarTipoAmbienteTrabajo() {
-        this._tipoAmbienteTrabajoService.listado().subscribe(
-            datos => {
-                this.tipoAmbienteTrabajoListado = datos;
-        }, error => {
-            this._mensajeService.cancelado(error, [{ name: '' }]);
-        });
     }
 }
