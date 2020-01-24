6083af26ea23af36e1bc7823e67c749070ea2664
areangonzalez <areangonzalez@gmail.com>
2020-01-24 12:47:17 -0300

    TG-695 se configura profesion con el abm dinamico

--- src/app/admin/vistas/vista-profesion.component.ts
+++ src/app/admin/vistas/vista-profesion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { ProfesionService } from 'src/app/core/services';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
     selector: 'admin-vista-profesion',
@@ -8,9 +10,10 @@ import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 export class VistaProfesionComponent implements OnInit {
   public titulos: string[] = [];
   public listado: any = [];
+  public objArmarForm: any = [];
 
   constructor(
-      private _router: Router, private _route: ActivatedRoute
+      private _router: Router, private _route: ActivatedRoute, private _profesionService: ProfesionService, private _toastrService: ToastrService
     ) {
   }
 
@@ -18,8 +21,54 @@ export class VistaProfesionComponent implements OnInit {
     this.renderTabla(this._route.snapshot.data['profesiones']);
   }
 
-  renderTabla(listaOficio: any) {
-    this.titulos = Object.keys(listaOficio[0]);
-    this.listado = listaOficio;
-  }
+  /**
+     * Separo los datos de la lista para obtener sus propiedades
+     * @param listado listado de oficios
+     */
+    renderTabla(listado: any) {
+      this.titulos = Object.keys(listado[0]);
+      this.listado = listado;
+      this.objArmarForm = listado[0];
+    }
+    /**
+     * guardado de un elemento nuevo o editado.
+     * @param datos datos a guardar
+     */
+    guardar(datos:any) {
+      if (datos !== false){
+        if (datos["id"] == 0){
+          this._profesionService.guardar(datos, 0).subscribe(
+            respuesta => {
+              this._toastrService.success('Se ha creado un nuevo oficio!!!');
+              this.refrescarListado();
+            }, error => { this._toastrService.error(error); });
+        }else{
+          this._profesionService.guardar(datos, datos["id"]).subscribe(
+            respuesta => {
+              this._toastrService.success('El oficio se ha editado correctamente!!!');
+              this.refrescarListado();
+          }, error => { this._toastrService.error(error); });
+        }
+      }
+    }
+    /**
+     * refresca el listado con nuevos datos
+     */
+    refrescarListado() {
+      this._profesionService.listarProfesiones().subscribe(
+        respuesta => {
+          this.listado = respuesta;
+        })
+    }
+    /**
+     * borra un elemento del listado
+     * @param id numero de identificador del elemento a borrar
+     */
+    borrar(id:number) {
+      this._profesionService.borrar(id).subscribe(
+        respuesta => {
+          this._toastrService.success("Se ha borrado el oficio correctamente.");
+          this.refrescarListado();
+        }, error => { this._toastrService.error(error); });
+    }
 }
--- src/app/core/services/profesion.service.ts
+++ src/app/core/services/profesion.service.ts
@@ -8,18 +8,44 @@ import { HttpParams } from '@angular/common/http';
 export class ProfesionService {
 
     constructor(private _apiService: ApiService) { }
-
+    /**
+     * Obtiene el listado de profesiones del servidor
+     */
     listarProfesiones() {
-
-       return this._apiService.get('/profesions');
+      return this._apiService.get('/profesions');
     }
-
+    /**
+     * busca una profesion por nombre
+     * @param nombre nombre de la profesion a buscar
+     */
     buscarPorNombre(nombre:string) {
       //let httpParams = new HttpParams();
       let httpParams = new HttpParams();
       httpParams = this._apiService.formatParams(httpParams, {'nombre':nombre});
 
       return this._apiService.get('/profesions', httpParams);
+    }
+
+    /**
+     * Crea/modifica los datos en el servidor
+     * @param params parametros utilizados para crear o modficar
+     * @param id identificador del objeto
+     */
+    guardar(params:any, id:number) {
+      if (id !== 0) {
+        // edito
+        return this._apiService.put('/oficios/' + id, params);
+      }else{
+        // creo
+        return this._apiService.post('/oficios', params);
+      }
+    }
+    /**
+     * borrado de un elemento por su id
+     * @param id identificador del elemento
+     */
+    borrar(id:number) {
+      return this._apiService.delete('/oficios/' + id);
     }
 
     /**
