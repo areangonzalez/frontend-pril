5289a4999806bdbaf2186e95bf1f8b357621b009
areangonzalez <areangonzalez@gmail.com>
2020-01-24 13:02:57 -0300

    TG-727 #Close - TG-695 #Done se configura tipo ambiente de trabajo con el abm dinamico

--- src/app/admin/vistas/vista-tipo-ambiente-trabajo.component.ts
+++ src/app/admin/vistas/vista-tipo-ambiente-trabajo.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { TipoAmbienteTrabajoService } from 'src/app/core/services';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
     selector: 'admin-vista-tipo-ambiente-trabajo',
@@ -8,9 +10,10 @@ import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 export class VistaTipoAmbienteTrabajoComponent implements OnInit {
   public titulos: string[] = [];
   public listado: any = [];
+  public objArmarForm: any = [];
 
   constructor(
-      private _router: Router, private _route: ActivatedRoute
+      private _router: Router, private _route: ActivatedRoute, private _tipoAmbienteTrabajo: TipoAmbienteTrabajoService, private _toastrService: ToastrService
     ) {
   }
 
@@ -18,8 +21,54 @@ export class VistaTipoAmbienteTrabajoComponent implements OnInit {
     this.renderTabla(this._route.snapshot.data['tipoAmbienteTrabajos']);
   }
 
-  renderTabla(listaOficio: any) {
-    this.titulos = Object.keys(listaOficio[0]);
-    this.listado = listaOficio;
-  }
+  /**
+     * Separo los datos de la lista para obtener sus propiedades
+     * @param listado listado de oficios
+     */
+    renderTabla(listado: any) {
+      this.titulos = Object.keys(listado[0]);
+      this.listado = listado;
+      this.objArmarForm = listado[0];
+    }
+    /**
+     * guardado de un elemento nuevo o editado.
+     * @param datos datos a guardar
+     */
+    guardar(datos:any) {
+      if (datos !== false){
+        if (datos["id"] == 0){
+          this._tipoAmbienteTrabajo.guardar(datos, 0).subscribe(
+            respuesta => {
+              this._toastrService.success('Se ha creado un nuevo oficio!!!');
+              this.refrescarListado();
+            }, error => { this._toastrService.error(error); });
+        }else{
+          this._tipoAmbienteTrabajo.guardar(datos, datos["id"]).subscribe(
+            respuesta => {
+              this._toastrService.success('El oficio se ha editado correctamente!!!');
+              this.refrescarListado();
+          }, error => { this._toastrService.error(error); });
+        }
+      }
+    }
+    /**
+     * refresca el listado con nuevos datos
+     */
+    refrescarListado() {
+      this._tipoAmbienteTrabajo.listado().subscribe(
+        respuesta => {
+          this.listado = respuesta;
+        })
+    }
+    /**
+     * borra un elemento del listado
+     * @param id numero de identificador del elemento a borrar
+     */
+    borrar(id:number) {
+      this._tipoAmbienteTrabajo.borrar(id).subscribe(
+        respuesta => {
+          this._toastrService.success("Se ha borrado el oficio correctamente.");
+          this.refrescarListado();
+        }, error => { this._toastrService.error(error); });
+    }
 }
--- src/app/core/services/tipo-ambiente-trabajo.service.ts
+++ src/app/core/services/tipo-ambiente-trabajo.service.ts
@@ -7,9 +7,32 @@ import { Observable } from 'rxjs';
 export class TipoAmbienteTrabajoService {
 
     constructor(private _apiService: ApiService) { }
-
+    /**
+     * Obtiene el listado de tipos de ambientes desde el servidor
+     */
     listado() {
         return this._apiService.get('/tipo-ambiente-trabajos');
+    }
+    /**
+     * Crea/modifica los datos en el servidor
+     * @param params parametros utilizados para crear o modficar
+     * @param id identificador del objeto
+     */
+    guardar(params:any, id:number) {
+      if (id !== 0) {
+        // edito
+        return this._apiService.put('/oficios/' + id, params);
+      }else{
+        // creo
+        return this._apiService.post('/oficios', params);
+      }
+    }
+    /**
+     * borrado de un elemento por su id
+     * @param id identificador del elemento
+     */
+    borrar(id:number) {
+      return this._apiService.delete('/oficios/' + id);
     }
 
     /**
