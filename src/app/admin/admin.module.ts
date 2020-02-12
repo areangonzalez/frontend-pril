588c2b18ebd56c52790670d6a71868a4d43ce853
areangonzalez <areangonzalez@gmail.com>
2020-02-12 14:00:14 -0300

    TG-738 #Closed se agrega el modulo shared

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,18 +1,19 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgModule, ModuleWithProviders, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { SharedModule } from "../shared";
 import { AdminRoutingModule } from "./admin-routing.module";
+
 import { AdminComponent } from "./admin.component";
 import {
   VistaOficioComponent, VistaPlanComponent, VistaProfesionComponent, VistaTipoAmbienteTrabajoComponent
 } from './vistas';
 import { AbmTablaComponent, AbmAgregarModalComponent, ModalContentAbmAgregar, AbmFormComponent, AbmBorrarModalComponent, ModalContentAbmBorrar } from "./abm";
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GestorUsuarioComponent, GuTablaComponent, ModalContentAgregarUsuario, GuAgregarUsuarioModalComponent, GuFormUsuarioComponent, GuFormAgenteComponent, GuDarRolPermisoModalComponent, ModalContentGuDarRolPermiso } from "./gestor-usuario";
 
 @NgModule({
     imports: [
-      CommonModule, NgbModule, FormsModule, ReactiveFormsModule,
+      CommonModule, NgbModule, SharedModule,
       AdminRoutingModule
     ],
     declarations: [
